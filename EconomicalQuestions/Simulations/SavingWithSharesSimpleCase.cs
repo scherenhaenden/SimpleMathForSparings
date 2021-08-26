f48db80986eda5c1f61aa23fa556b40e7ee06151
Eddie Krill <deltorodata@yahoo.it>
2021-08-26 11:54:39 +0200

    Changes:
    - Tell me current winnings of the year
    - Tell me cumulated winnings

--- EconomicalQuestions/Simulations/SavingWithSharesSimpleCase.cs
+++ EconomicalQuestions/Simulations/SavingWithSharesSimpleCase.cs
@@ -8,12 +8,14 @@ namespace EconomicalQuestions.Simulations
         {
 
             double spares = 0;
+            double ownInvestment = 0;
             List<string> sumOfOutPuts = new List<string>();
             
 
             for (int i = 0; i < years; i++)
             {
                 spares += yearlySaving;
+                ownInvestment += yearlySaving;
                 var possibleGaining = spares * averageDividends;
 
                 var difference = possibleGaining - spares;
@@ -22,16 +24,16 @@ namespace EconomicalQuestions.Simulations
 
                 spares = spares + difference;
                 
-                sumOfOutPuts.Add(CreateString(i+1, spares));
+                sumOfOutPuts.Add(CreateString(i+1, spares, ownInvestment, (spares - ownInvestment), difference));
             }
 
             return sumOfOutPuts;
         }
 
 
-        public string CreateString(int year, double money)
+        public string CreateString(int year, double money, double ownInvestment, double winningsFromPercentage, double winningsCurrentYear)
         {
-            return $"Year number {year} i would have so much money: {money}";
+            return $"Year number {year} i would have so much money: {money}. OwnInvestment {ownInvestment}. Cumulated winnings {winningsFromPercentage}. Winnings current year {winningsCurrentYear}";
         }
         
         
